9cdf7328b46c98fd9962cc5157fe8cc33a6e3c65
musapads <106446782+musapads@users.noreply.github.com>
2022-08-26 14:47:57 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    embedColor: "RANDOM",
+    admins: ["884451720510451753"],
+    serverId: "892850841671843881",
+    mongoURL: "mongodb+srv://musap123:musap123@cluster0.h6edv.mongodb.net/?retryWrites=true&w=majority",
+    status: ["Non Serviant", "Non Serviant"],
+
+    muteLimit: 10,
+    banLimit: 5,
+    vmuteLimit: 10,
+    jailLimit: 6,
+
+    tag: "ℭ",
+    tagsıztag: ".",
+
+    booster: "929263903102365727",
+    unregister: "1006320018834866227",
+    erkek: "993261146423431288",
+    kız: "993261146423431288",
+
+    muted: "1006594209677066371",
+    cezalı: "1012396476418236426",
+    şüpheli: "1006997581227888790",
+
+    banHammer: ["1012398031125086268", ""],
+    registerHammer: ["1006321390242246696", ""],
+    muteHammer: ["1012398755573682257", ""],
+    voiceMuteHammer: ["1012399307225321503", ""],
+    jailHammer: ["1012399316582797464", ""],
+    transportHammer: ["", ""],
+
+    banLog: "1012392582816485577",
+    muteLog: "1012392493695901836",
+    vmuteLog: "1012392645823303781",
+    jailLog: "1012392683114856589",
+    botkomut: "1006240849379721286",
+
+
+    ok: "1012393151232737320",
+    no: "1012393201639882812",
+    okname: "Evet",
+    noname: "Hayir"
+}
